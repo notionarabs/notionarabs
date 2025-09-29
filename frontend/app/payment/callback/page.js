4a52a7feafc0e4436805da0f634f2bf80f452e9e
Hazem <114850797+hazemyasserprg@users.noreply.github.com>
2025-09-29 16:22:14 +0300

    Fix LoadingIndicator import path in payment callback page

--- frontend/app/payment/callback/page.js
+++ frontend/app/payment/callback/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import LoadingIndicator from '../../components/LoadingIndicator';
+import LoadingIndicator from '../../../components/LoadingIndicator';
 
 export default function PaymentCallback() {
   const searchParams = useSearchParams();

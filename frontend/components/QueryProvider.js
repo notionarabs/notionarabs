fa08f24459999c79ed45407b3ad0c94e038e2dc8
hazemyasserprg <hazemyasser911@gmail.com>
2025-10-25 04:08:02 +0300

    Fix QueryProvider: Remove react-query-devtools import

--- frontend/components/QueryProvider.js
+++ frontend/components/QueryProvider.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import { queryClient } from '../lib/queryClient';
 
@@ -9,7 +8,6 @@ export function QueryProvider({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
